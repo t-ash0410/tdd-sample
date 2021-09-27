f94a8c5959b8ea4113f0408036dd86e05165b768
t-ash0410 <t_ash0410@yahoo.co.jp>
2021-09-28 07:58:53 +0900

    Setting gRPC dial options.

--- backend/proto/clients/handlers/handler_base.go
+++ backend/proto/clients/handlers/handler_base.go
@@ -2,11 +2,14 @@ package handlers
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"log"
 	"net/http"
 	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 type HandlerBase struct {
@@ -25,11 +28,31 @@ func (h *HandlerBase) ExecuteRpc(res http.ResponseWriter, f func(ctx context.Con
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
-	conn, err := grpc.DialContext(ctx, h.rpcAddress, grpc.WithInsecure())
+	opts, err := h.createConnectionOpts()
+	if err != nil {
+		h.Write500Error(res, err)
+		return
+	}
+
+	conn, err := grpc.DialContext(ctx, h.rpcAddress, opts...)
 	if err != nil {
 		h.Write500Error(res, err)
 		return
 	}
 	defer conn.Close()
 	f(ctx, conn)
+}
+
+func (h *HandlerBase) createConnectionOpts() ([]grpc.DialOption, error) {
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithAuthority(h.rpcAddress))
+	systemRoots, err := x509.SystemCertPool()
+	if err != nil {
+		return nil, err
+	}
+	cred := credentials.NewTLS(&tls.Config{
+		RootCAs: systemRoots,
+	})
+	opts = append(opts, grpc.WithTransportCredentials(cred))
+	return opts, nil
 }
