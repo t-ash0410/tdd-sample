0668faa17a985e389ef97a620d61cb8d401535ec
t-ash0410 <t_ash0410@yahoo.co.jp>
2021-09-28 05:47:06 +0900

    Create mock rpc server

---
+++ backend/cmd/mock/rpc_todo.go
@@ -0,0 +1,67 @@
+package mock
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"time"
+
+	"github.com/t-ash0410/tdd-sample/backend/internal/api/todo/entities"
+	"github.com/t-ash0410/tdd-sample/backend/internal/api/todo/interfaces"
+	"github.com/t-ash0410/tdd-sample/backend/internal/api/todo/usecases"
+	pb "github.com/t-ash0410/tdd-sample/backend/proto/generates/todo"
+	"github.com/t-ash0410/tdd-sample/backend/proto/servers"
+	"google.golang.org/grpc"
+)
+
+type Repository struct {
+}
+
+func (repo *Repository) List(result *[]entities.Task) error {
+	*result = append(*result, entities.Task{
+		Id:          "id",
+		Name:        "name",
+		Description: "description",
+		UpdatedAt:   time.Now(),
+	})
+	return nil
+}
+
+func (repo *Repository) Add(task entities.Task) error {
+	return nil
+}
+
+type UsecaseFactory struct {
+}
+
+func (f *UsecaseFactory) CreateList(ctx context.Context) (interfaces.IListUsecase, func()) {
+	return usecases.NewListUsecase(&Repository{}), func() {}
+}
+
+func (f *UsecaseFactory) CreateAdd(ctx context.Context) (interfaces.IAddUsecase, func()) {
+	return usecases.NewAddUsecase(&Repository{}), func() {}
+}
+
+const (
+	portKey = "PORT"
+)
+
+func main() {
+	port := os.Getenv(portKey)
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	factory := &UsecaseFactory{}
+	server := servers.NewTodoServer(factory)
+
+	s := grpc.NewServer()
+	pb.RegisterTodoServer(s, server)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+	log.Print("listen...")
+}
